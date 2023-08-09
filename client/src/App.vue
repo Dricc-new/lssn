b4758819bc7315a6e5e2ee71d2a5cb7f6f6d53b3
Dricc-new <diegoramoncc17@gmail.com>
2023-08-09 17:27:04 -0400

    home page created

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,5 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <main class="container max-sm:p-2 mx-auto">
+    <RouterView />
+  </main>
 </template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,7 +2,9 @@
 </script>
 
 <template>
-  <main>
-    <h1></h1>
-  </main>
+  <section class="flex flex-col gap-10 h-screen justify-center sm:items-start">
+    <h1 class="text-center sm:text-left bg-clip-text text-transparent bg-gradient-to-r from-sky-600 to-green-700 text-5xl">Welcome to my app</h1>
+    <p class="text-center sm:text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam pariatur, porro harum accusamus itaque corrupti
+      voluptatum perferendis eos? Molestiae, dicta. Ratione, et suscipit modi eaque eligendi eum sunt impedit in?</p>
+  </section>
 </template>
