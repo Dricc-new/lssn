e31a9e54bdbb767a44a49c92b2f3f96752fe25c7
Dricc-new <diegoramoncc17@gmail.com>
2023-08-09 17:58:35 -0400

    Login terminated

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,3 +1,10 @@
 <template>
-    
+    <section class="flex flex-col justify-center p-4">
+        <div class="bg-stone-200 rounded p-4 flex flex-col gap-2">
+            <h1 class="text-stone-900 font-bold text-2xl">Login</h1>
+            <a href="#" class="px-2 p-1 bg-gradient-to-br from-blue-600 to-stone-800  rounded flex gap-2 justify-center items-center ">
+                <i class="fab fa-linkedin text-2xl"></i>Continue with Linkedin
+            </a>
+        </div>
+    </section>
 </template>
